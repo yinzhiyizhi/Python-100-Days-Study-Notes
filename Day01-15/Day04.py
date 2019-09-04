793bc96e0a12873a46e925177880a4d1771fc59d
yinzhiyizhi <yinzhiyizhi@gmail.com>
2019-09-04 17:17:36 +0800

    finished Day04

---
+++ Day01-15/Day04.py
@@ -0,0 +1,86 @@
+from math import sqrt
+
+"""
+输出乘法口诀表(九九表)
+"""
+
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print('%d*%d=%d' % (i, j, i * j), end='\t')
+    print()
+
+
+"""
+输入一个正整数判断它是不是素数
+"""
+
+num = int(input('请输入一个正整数：'))
+end = int(sqrt(num))
+is_prime = True
+for x in range(2, end + 1):
+    if num % x == 0:
+        is_prime = False
+        break
+if is_prime and num != 1:
+    print('%d是素数' % num)
+else:
+    print('%d不是素数' % num)
+
+
+"""
+输入两个正整数计算最大公约数和最小公倍数
+"""
+
+x = int(input('x = '))
+y = int(input('y = '))
+if x > y:
+    x, y = y, x
+for factor in range(x, 0, -1):
+    if x % factor == 0 and y % factor == 0:
+        print('%d和%d的最大公约数是%d' % (x, y, factor))
+        print('%d和%d的最大公倍数是%d' % (x, y, x * y // factor))
+        break
+
+        
+"""
+打印各种三角形图案
+
+*
+**
+***
+****
+*****
+
+    *
+   **
+  ***
+ ****
+*****
+
+    *
+   ***
+  *****
+ *******
+*********
+"""
+
+row = int(input('请输入行数： '))
+for i in range(row):
+    for _ in range(i + 1):
+        print('*', end='')
+    print()
+
+for i in range(row):
+    for j in range(row):
+        if j < row - i - 1:
+            print(' ', end='')
+        else:
+            print('*', end='')
+    print()
+
+for i in range(row):
+    for _ in range(row - i - 1):
+        print(' ', end='')
+    for _ in range(2 * i + 1):
+        print('*', end='')
+    print()
